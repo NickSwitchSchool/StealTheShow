ab847683650de5b014b989399afb40520cfbfb8d
NickSwitchSchool <97087180@st.deltion.nl>
2024-02-05 11:47:30 +0100

    Level generation
    + Added the beginning of the code to spawn the level nodes in random order with restrictions

--- CircusSurfers/Assets/Scripts/LevelGeneration.cs
+++ CircusSurfers/Assets/Scripts/LevelGeneration.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public GameObject startNode;
+    public Transform startNodePos;
+    public float minimumDistanceToLastNode;
+    GameObject lastSpawnedNode;
+    float distanceBetweenPlayerAndLastNode;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (player == null)
+        {
+            Debug.LogWarning("Player is not assigned!");
+            return;
+        }
+        else if (startNode == null)
+        {
+            Debug.LogWarning("Start node is not assigned!");
+            return;
+        }
+
+        if (lastSpawnedNode == null)
+        {
+           lastSpawnedNode = Instantiate(startNode, startNodePos);
+        }
+        else if (Vector3.Distance(player.transform.position, lastSpawnedNode.transform.position) < minimumDistanceToLastNode)
+        {
+            int _index = Random.Range(0, lastSpawnedNode.GetComponent<LevelNode>().possibleNextNodes.Length);
+        }
     }
 }
---
+++ CircusSurfers/Assets/Scripts/LevelNode.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelNode : MonoBehaviour
+{
+    public GameObject[] possibleNextNodes;
+    public Transform nextNodeLocation;
+}
